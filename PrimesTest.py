3cba854cd34c1fc6e4c524e21d5cdd463c0e6393
Your Name <you@example.com>
2019-02-15 16:30:21 +0300

    Structure2

--- PrimesTest.py
+++ PrimesTest.py
@@ -1,8 +1,30 @@
+import time
 
-nums = [i+1 for i in range(1, 10000)]
-for n in nums:
-    for m in nums[1:]:
-        if n!=m and m % n == 0:
-            nums.remove(m)
+class PrimeNumbers:
+    N = 10000
+    nums = [i+1 for i in range(1, N)]
+    boolNums = [True for i in range(1, N)]
 
-print(nums)
+    #Медленный метод
+    def SlowMethod(N=N):
+        start_time= time.time()
+        nums = [i+1 for i in range(1, N)]
+        for n in nums:
+            for m in nums[1:]:
+                if n!=m and m % n == 0:
+                    nums.remove(m)
+        #print(nums)
+        print('Slow Time: '+str(round(time.time()-start_time, 6)))
+
+    #Решето
+    def Sieve(_nums = nums, _N=N):
+        start_time= time.time()
+        for n in range(2, _N):
+            for m in range(n*n, _N, n):
+                    _nums[m]=-1
+                    #_nums.remove(m)
+        print(_nums)
+        print('Sieve Time: '+str(round(time.time()-start_time, 6)))
+
+#PrimeNumbers.SlowMethod()
+PrimeNumbers.Sieve()
